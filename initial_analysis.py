585b923bd06d0f0d0a68bfa14a23c73a5d97efbd
Ismael Robles-Razzaq <iroblesrazzaq@midway3-login3.rcc.local>
2024-05-09 13:33:13 -0500

    xgboost file fixes, gpu shell file

--- initial_analysis.py
+++ initial_analysis.py
@@ -1,36 +1,23 @@
 from pathlib import Path
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
 from xgboost import XGBClassifier
+import sys
 
-ignore = ["Mass(1)", "Mass(2)", "Eccentricity@DCO", "SemiMajorAxis@DCO", "Coalescence_Time"]
-DATADIR = Path("/project/dfreedman/colmt/UChicago-AI-in-Science-Hackathon/stellar-paleontology-data/")
+if len(sys.argv) > 1:
+    DATADIR = Path(sys.argv[1])
+else:
+    DATADIR = Path("/project/dfreedman/colmt/UChicago-AI-in-Science-Hackathon/stellar-paleontology-data/")
 
 def load_classification_data(datadir=DATADIR):
+    ignore = ["Mass(1)", "Mass(2)", "Eccentricity@DCO", "SemiMajorAxis@DCO", "Coalescence_Time"]
     double_compact_objects = pd.read_pickle(datadir / "compas-data.pkl")
     for key in ignore:
         if key in double_compact_objects:
             del double_compact_objects[key]
     return double_compact_objects
-
-def df_stats(data_df):
-    pd.set_option('display.max_columns', None)
-    print(data_df.describe())
-    print(data_df.head(5))
-    print(data_df.columns)
-    col_names = list(data_df.columns)
-    print(col_names)
-    print("column value counts:")
-    for col in col_names:
-        print(data_df[col].value_counts())
-    print(data_df.isna().sum())
 
 def statistics(confusion: np.ndarray, verbose=True) -> float:
     """
@@ -56,8 +43,18 @@ def build_model(data_df):
     data_df.drop(['Merges_Hubble_Time'], axis=1, inplace=True)
     X = data_df
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
-    xgb_model_1 = XGBClassifier(random_state=0, n_estimators=500, early_stopping_rounds=5, learning_rate=0.05)
-    xgb_model_1.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=False)
+    xgb_params = {
+        'objective': 'binary:logistic',
+        'eval_metric': 'logloss',
+        'use_label_encoder': False,
+        'tree_method': 'gpu_hist',
+        'predictor': 'gpu_predictor',
+        'n_estimators': 500,
+        'early_stopping_rounds': 5,
+        'learning_rate': 0.05
+    }
+    xgb_model_1 = XGBClassifier(**xgb_params)
+    xgb_model_1.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=True)
     predictions_1 = xgb_model_1.predict(X_valid)
     return xgb_model_1, predictions_1, X_valid, y_valid
 
@@ -69,6 +66,5 @@ def evaluate_model(model, predictions, y_valid):
 
 if __name__ == "__main__":
     data_df = load_classification_data()
-    df_stats(data_df)
     model, predictions, X_valid, y_valid = build_model(data_df)
-    evaluate_model(model, predictions, y_valid)
+    confusion, balanced_acc = evaluate_model(model, predictions, y_valid)
